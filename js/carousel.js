4e8fe8ecf54b5aef0bc15b731e3ccf41cf28f5f7
Jared De la O <jareddelao@MacBook-Air-de-Jared.local>
2020-05-05 02:34:23 -0500

    changes

--- js/carousel.js
+++ js/carousel.js
@@ -1,4 +1,17 @@
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.swiper-inicio', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+        clickable: true
+    }
+});
+
+var swiper = new Swiper('.swiper-sistema', {
     loop: true,
     navigation: {
       nextEl: '.swiper-button-next',
@@ -14,7 +27,11 @@ var swiper = new Swiper('.swiper-container', {
 var swiper2 = new Swiper('.swiper-clientes', {
     slidesPerView: 2,
     spaceBetween: 10,
-    // loop: true,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
     pagination: {
       el: '.pagination-clientes',
       clickable: true,
@@ -35,3 +52,46 @@ var swiper2 = new Swiper('.swiper-clientes', {
         },
     }
 });
+
+
+// scripts smooth scroll
+$(".routers .servicios").click(function() {
+  $('html, body').animate({
+      scrollTop: $(".cards-servicios").offset().top - 70
+  }, 500);
+});
+$(".routers .estudios").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#estudios").offset().top - 70
+  }, 500);
+});
+$(".routers .clientes").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#clientes").offset().top
+  }, 500);
+});
+$(".routers .contacto").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#contacto").offset().top
+  }, 500);
+});
+
+// Sidebar
+var open = false;
+
+function fromSidebar(e) {
+  closeNav();
+  $('html, body').animate({
+    scrollTop: $(e).offset().top
+  }, 500);
+}
+
+function openNav() {
+  open = true;
+  document.getElementById("mySidebar").style.width = "250px";
+}
+
+function closeNav() {
+  open = false;
+  document.getElementById("mySidebar").style.width = "0";
+}
